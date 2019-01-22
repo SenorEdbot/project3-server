102b613616c38ee7382ed35305ee24fe7c7fda87
Jason Michael <michael.jason@outlook.com>
2019-01-21 18:08:41 -0600

    added cors whitelist

--- server.js
+++ server.js
@@ -4,12 +4,26 @@ const PORT = process.env.PORT || 3001
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/midWaste";
 const server = express()
 const routes = require('./routes')
+const cors = reauire('cors')
 
 // Middleware
 server.use(express.urlencoded({ extended: false }))
 server.use(express.json())
 server.use(express.static('client/dist'))
 
+// Set up a CORS whitelist...
+const whitelist = ['https://midwaste.herokuapp.com', 'http://localhost:3000']
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback (new Error('Not allowed by CORS'))
+    }
+  }
+}
+// ...and check against it.
+server.use(cors(corsOptions))
 
 // Routes
 server.use(routes)
